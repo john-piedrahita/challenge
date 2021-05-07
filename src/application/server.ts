3007ac9243d52dd70aa07c9a348ae5ed399da25f
John Piedrahita <john.piedrahita@insoftar.com>
2021-05-07 09:52:04 -0500

    feat: implementacion y configuracion del gestor de base de datos en el proyecto

--- src/application/server.ts
+++ src/application/server.ts
@@ -1,9 +1,14 @@
 import 'module-alias/register'
-import {PORT} from "@/application/config/environment";
+import fs from "fs"
+import dotenv from "dotenv"
+import {MongoHelper} from "@/infrastructure/driven-adapters/adapters/mongo-adapter/mongo-helper";
+import {MONGODB_URI, PORT} from "@/application/config/environment";
 
-async function main() {
+
+if (fs.existsSync(".env")) dotenv.config({ path: ".env" })
+
+MongoHelper.connect(MONGODB_URI).then(async () => {
+    console.log("Connected mongoDB")
     const app = (await import('./config/app')).default
     app.listen(PORT, () => console.log("Server an running on port: " + PORT))
-}
-
-main().then(r => r).catch(e => console.log(e))
+}).catch(err => console.log(err))
